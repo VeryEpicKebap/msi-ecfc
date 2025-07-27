ebd574aec942111e1bb49b3e91e066c305717220
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-07-27 16:25:24 +0300

    optimizations

--- src/ecfc.c
+++ src/ecfc.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/io.h> 
+#include <sys/io.h>
+#include <time.h>
+#include <sched.h>
 
 #define EC_CMD_PORT  0x66
 #define EC_DATA_PORT 0x62
+#define EC_FAN_REG   0xEE
+#define EC_FAN_VAL   0x54
 
 void ec_wait_input() {
-    while (inb(EC_CMD_PORT) & 0x02); 
+    while (inb(EC_CMD_PORT) & 0x02);
 }
 
 void ec_wait_output() {
-    while (!(inb(EC_CMD_PORT) & 0x01)); 
+    while (!(inb(EC_CMD_PORT) & 0x01));
 }
 
 void ec_write(unsigned char addr, unsigned char value) {
     ec_wait_input();
-    outb(0x81, EC_CMD_PORT);   
+    outb(0x81, EC_CMD_PORT);    
     ec_wait_input();
-    outb(addr, EC_DATA_PORT);  
+    outb(addr, EC_DATA_PORT);    
     ec_wait_input();
     outb(value, EC_DATA_PORT); 
+}
+
+unsigned char ec_read(unsigned char addr) {
+    ec_wait_input();
+    outb(0x80, EC_CMD_PORT);     
+    ec_wait_input();
+    outb(addr, EC_DATA_PORT);
+    ec_wait_output();
+    return inb(EC_DATA_PORT);
 }
 
 int main() {
@@ -27,10 +40,15 @@ int main() {
         perror("ioperm");
         return 1;
     }
+    struct sched_param sp = {0};
+    sched_setscheduler(0, SCHED_IDLE, &sp);
+    struct timespec delay = {0, 5 * 1000000};
 
     while (1) {
-        ec_write(0xEE, 0x54); // T is the highest EC fan value for the MSI CX640. Adjust as needed.
-        usleep(2000);       
+        if (ec_read(EC_FAN_REG) != EC_FAN_VAL) {
+            ec_write(EC_FAN_REG, EC_FAN_VAL);
+        }
+        nanosleep(&delay, NULL);
     }
 
     return 0;
